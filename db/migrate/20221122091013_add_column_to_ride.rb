460fba7a08bca6c4af6bfc50bbe0ecfbe585d16d
iSaacSigei <kiplangatisack.is@gmail.com>
2022-11-22 12:20:24 +0300

    Completed assignment

---
+++ db/migrate/20221122091013_add_column_to_ride.rb
@@ -0,0 +1,8 @@
+class AddColumnToRide < ActiveRecord::Migration[6.1]
+  def change
+    add_column :rides, :taxi_id,:integer
+    add_column :rides, :passenger_id,:integer
+
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
